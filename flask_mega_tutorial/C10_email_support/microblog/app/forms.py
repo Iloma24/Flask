75f93226df98be3820c9391e56e179104aa2b2dc
joma ubuntu <jomalaki5@gmail.com>
2026-05-31 12:12:15 +0300

    mega_tutorial_after_C10

---
+++ flask_mega_tutorial/C10_email_support/microblog/app/forms.py
@@ -0,0 +1,152 @@
+# в данном модуле, с помощью классов, прописываются вэб-формы (поля ввода) для входа пользователя в систему и для регистрации
+# нового пользователя, используемые в приложении и их логика. Созданные здесь формы имеют соответствующие шаблоны в папке
+# с шаблонами, и эти шаблоны позволяют отображать формы
+
+from flask_wtf import FlaskForm  # импорт базового встроенного класса вэб-формам
+from wtforms import StringField, PasswordField, BooleanField, SubmitField  # импорт встроенных полей
+from wtforms import TextAreaField  # многострочное поле
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length  # импорт валидаторов ввода значений в поля
+                            # для регистрации. Length - валидатор длинны строки
+import sqlalchemy as sa  # для работы с БД (запросы, функции и т.д.)
+from app import db  # импорт БД
+from app.models import User  # импорт модели таблицы Пользователи
+
+
+class LoginForm(FlaskForm):  # создание пользовательского класса из встроенного
+    """
+    Данный класс является кастомным, дочерним от встроенного FlaskForm. Представляет из себя форму для входа пользователя
+     в систему. Имеет 4 атрибута, из 3 поля (2 обязательных для заполнения) для заполнения и один чекбокс. Прорисовка
+     этой формы выполнена в шаблоне login.html
+    """
+    # далее создаются 4 класса - типы полей. Первый параметр каждого класса - описание (название) поля
+    username = StringField('Username', validators=[DataRequired()])  # поле - имя пользователя, обязательное (DataRequired)
+                                                                        # для заполнения поле, состоящее из строки
+    password = PasswordField('Password', validators=[DataRequired()])  # поле - пароль, обязательное для заполнения
+    remember_me = BooleanField('Remember me')  # поле - запомнить меня, не обязательное, с булевым значением
+    submit = SubmitField('Sign in')  # чекбокс, не обязательный
+
+
+class RegistrationForm(FlaskForm):
+    """
+    Данный класс кастомный, имеет 5 атрибутов и два метода, является дочерним от встроенного в библиотеку flask_wtf
+    класса FlaskForm создает регистрационную форму, состоящую из нескольких полей. Каждое поле в виде атрибута класса.
+    Обеспечивается проверка (валидация) типа вводимых данных. Имеет два метода, каждый из них соответственно проверяет
+    свободны ли введенные пользователем имя и имейл. Для отображения этой формы в папке с шаблонами нужно реализовать
+    соответствующий шаблон
+    """
+    username = StringField('Username', validators=[DataRequired()])  # поле для регистрации имени пользователя.
+                                    # Username - заголовок, validators=[DataRequired()] - влидация чтобы поле не было пустым
+
+    email = StringField('Email', validators=[DataRequired(), Email()])  # поле для регистрации имейла. Email() -
+                                    # специальный валидатор для имейла. !!! требует доп установки: pip install email-validator
+
+    password = PasswordField('Password', validators=[DataRequired()])  # поле для регистрации пароля. С заголовком
+                                    # и одним валидатором
+
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])  # поле подтверждения пароля. Заголовок
+                                    # и два валидатора. EqualTo - валидатор сравнивает значения двух полей
+
+    submit = SubmitField('Register')  # кнопка отправки формы для регистрации с надписью "Register"
+
+    # !!! ВАЖНО !!! Далее я создаю два пользовательских валидатора. Валидаторы создаваемые по шаблону validate_<название_поля>
+    # WTForms использует их в качестве пользовательских валидаторов и вызывает их в дополнение к стандартным валидаторам.
+    def validate_username(self, username):
+        """
+        Этот метод проверяет существует ли в БД пользователь с таким именен, если существует, вызывает исключение с
+        соответствующим сообщением
+        :param username: имя пользователя введенное в поле для регистрации
+        :return:
+        """
+        user = db.session.scalar(sa.select(User).where(
+            User.username == username.data))
+        if user is not None:
+            raise ValidationError('Please use a different username!')
+
+    def validate_email(self, email):
+        """
+        Этот метод проверяет существует ли в БД пользователь с таким имейлом, если существует, вызывает исключение с
+        соответствующим сообщением
+        :param email: имейл пользователя введенный в поле для регистрации
+        :return:
+        """
+        user = db.session.scalar(sa.select(User).where(
+            User.email == email.data))
+        if user is not None:
+            raise ValidationError('Please use a different email address!')
+
+
+class EditProfileForm(FlaskForm):
+    """
+    Это класс редактора профиля пользователя (форма). Состоит из нескольких полей для заполнения. Задача этого класса -
+    создать форму для редактирования профиля с «умной» проверкой уникальности имени. Главная проблема, которую он решает:
+    как не выдавать ошибку «это имя уже занято», когда пользователь сохраняет свое же текущее имя.
+    """
+    username = StringField('Username', validators=[DataRequired()])  # настройка поля "имя пользователя"
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])  # настройка поля "о себе".
+                                # TextAreaField - многострочное поле. Валидатор Length - проверяет соблюдение заданной
+                                # длинны строки (измерение в символах)
+    submit = SubmitField('Submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        """
+        original_username - это имя пользователя получаемое от встроенного класса current_user (содержит имя текущего
+        пользователя), эта передача происходит при создании формы в функции просмотра edit_profile в файле routes.py.
+        Аргумент original_username прописывается отдельно перед неименованными и именованными аргументами, поскольку он
+        далее прописывается как переменная экземпляра
+        :param original_username: текущее имя пользователя
+        :param args: любые неименованные аргументы
+        :param kwargs: именованные аргументы
+        """
+        super().__init__(*args, **kwargs)
+        self.original_username = original_username  # создается переменная экземпляра в которой сохраняется текущее имя
+                                                # пользователя, т.е. имя пользователя на момент входа на страницу
+                                                # редактирования профиля, дальше в методе validate_username, это имя будет
+                                                # сравниваться с именем в поле "username" формы
+
+    def validate_username(self, username):
+        """
+        Кастомный валидатор, который проверяет существует ли в БД заданное имя пользователя
+        :param username: это имя пользователя введенное в поле формы
+        :return:
+        """
+        if username.data != self.original_username:  # сравнивается имя пользователя из поля формы и имя пользователя
+                                                    # переданное при инициализации формы. Если пользователь в поле формы
+                                                    # изменил имя, то далее выполняется запрос в БД
+            user = db.session.scalar(sa.select(User).where(User.username == self.username.data))  # поиск в БД пользователя
+                                                    # с тамким же именем
+            if user is not None:  # если такое имя пользователя уже есть
+                raise ValidationError('Please use a different username!')
+
+
+class EmptyForm(FlaskForm):
+    """
+    Данный класс представляет пустую форму (кнопку) со скрытым CSRF-токеном для оформления подписки/отписки пользователя
+    """
+    submit = SubmitField('Submit')
+
+
+class PostForm(FlaskForm):
+    """
+    Этот класс представляет из себя - форму для отправки сообщений в блог
+    """
+    post = TextAreaField('Say something', validators=[DataRequired(), Length(min=1, max=140)])  # поле для ввода сообщения
+    submit = SubmitField('Submit')  # подтверждение отправки сообщения
+
+
+class ResetPasswordRequestForm(FlaskForm):
+    """
+    Форма запроса сброса пароля. Не самого сброса пароля!
+    """
+    email = StringField('Enter your email', validators=[DataRequired(), Email()])  # настройка поля ввода почты
+    submit = SubmitField('Request Password Reset')  # кнопка подтверждения отправки введенных данных
+
+
+class ResetPasswordForm(FlaskForm):
+    """
+    Это форма для сброса пароля
+    """
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Request Password Reset')
+
---
+++ flask_mega_tutorial/C10_email_support/microblog/tests.py
@@ -0,0 +1,131 @@
+# этот код представляет собой набор модульных тестов (unit tests) для приложения. Он проверяет логику работы модели
+# пользователя (User) и системы подписок. Ценность этих тестов в том, что они проверяют работу приложения после внесения
+# изменений в него и до его деплоя устранять ошибки
+import os  # для работы с операционной системой компьютера
+os.environ['DATABASE_URL'] = 'sqlite://'  # здесь устанавливается переменная окружения, которая говорит приложению использовать
+                        # базу данных SQLite в оперативной памяти, а не обычную БД, которая используется для разработки.
+                        # Это идеально для тестов: они проходят мгновенно, а данные стираются сразу после завершения, не
+                        # засоряя компьютер
+
+from datetime import  datetime, timezone, timedelta  # для работы с датами и временем
+import unittest  # импорт инструментов тестирования
+from app import appObj, db # импорт приложения и БД
+from app.models import User, Post  # импорт проверяемых классов
+
+
+class UserModelCase(unittest.TestCase):
+    """
+    UserModelCase - это кастомный класс, унаследованный из встроенного. Это кастомный тестировщик, он тестирует работу
+    методов моделей User и Post из models.py.
+    """
+    def setUp(self):
+        """
+        Метод запускается перед каждым тестом, создает контекст приложения и отправляет его. Это гарантирует, что экземпляр
+        приложения Flask вместе с его конфигурационными данными будет доступен для расширений Flask. Метод также создает
+        чистые таблицы в БД
+        :return:
+        """
+        self.app_context = appObj.app_context()  # создается контекст приложения
+        self.app_context.push()  # отправление/запуск контекста приложения
+        db.create_all()  # создает таблицы в БД, указанном выше в переменной окружения, на основе моделей описанных в
+                        # коде (в данном случае это User и Post)
+
+    def tearDown(self):
+        """
+        Метод запускается после каждого теста. Удаляет сессию и очищает базу данных, чтобы тесты не влияли друг на друга.
+        :return:
+        """
+        db.session.remove()  # очищает и закрывает текущую сессию взаимодействия с базой данных
+        db.drop_all()  # удаление таблиц и все данные из БД
+        self.app_context.pop()  # завершение существования контекста приложение, т.е. обратное тому что делает self.app_context.push()
+
+    # дальше начинаются непосредственно тесты
+    def test_password_hashing(self):
+        """
+        Данный тест проверяет соответствие введенного пароля заданному паролю
+        :return:
+        """
+        u = User(username='susan', email='susan@example.com')  # создание пользователя
+        u.set_password('cat')  # создание пароля
+        self.assertFalse(u.check_password('dog'))  # проверяется неверный ввод пароля
+        self.assertTrue(u.check_password('cat'))  # проверяется верный ввод пароля
+
+    def test_avatar(self):
+        """
+        Данный тест проверяет генерацию аватарок через сервис Gravatar. Код сравнивает созданную ссылку с ожидаемой
+        (хеш от email).
+        :return:
+        """
+        u = User(username='john', email='john@example.com')
+        self.assertEqual(u.avatar(128), ('https://www.gravatar.com/avatar/'
+                                         'd4c74594d841139328695756648b6bd6'
+                                         '?d=identicon&s=128'))  #
+
+    def test_follow(self):
+        u1 = User(username='john', email='john@example.com')  # создание пользователя 1 (п1)
+        u2 = User(username='susan', email='susan@example.com')  # создание пользователя 2 (п2)
+        db.session.add(u1)  # добавление пользователя в БД
+        db.session.add(u2)  #
+        db.session.commit()  # исполнение выполненных изменений
+        following = db.session.scalars(u1.following.select()).all()  # получение из БД списка пользователей на которых подписан пользователь 1
+        followers = db.session.scalars(u2.followers.select()).all()  # получение из БД списка пользователей подписанных на пользователя 2
+        self.assertEqual(following, [])  # проверяется что п1 не подписан на п2
+        self.assertEqual(followers, [])  # проверяется что п2 не имеет подписчика п1
+
+        u1.follow(u2)  # п1 подписывается на п2
+        db.session.commit()  # исполнение изменения
+        self.assertTrue(u1.is_following(u2))  # проверка что п1 подписан на п2
+        self.assertEqual(u1.following_count(), 1)  # проверка что п1 подписан на одного пользователя
+        self.assertEqual(u2.followers_count(), 1)  # проверка что у п2 один подписчик
+        u1_following = db.session.scalars(u1.following.select()).all()  # получение всех пользователей, на которые подписан п1
+        u2_followers = db.session.scalars(u2.followers.select()).all()  # получение всех подписчиков п2
+        self.assertEqual(u1_following[0].username, 'susan')  # проверка что п1 подписан на пользователя по имени susan
+        self.assertEqual(u2_followers[0].username, 'john')  # проверка что п2 имеет подписчика по имени john
+
+        u1.unfollow(u2)  # п1 отписывается от п2
+        db.session.commit()  # исполнение изменений
+        self.assertFalse(u1.is_following(u2))  # проверка что п1 не подписан на п2
+        self.assertEqual(u1.following_count(), 0)  # проверка что п1 ни на кого не подписан
+        self.assertEqual(u2.followers_count(), 0)  #проверка что п2 не имеет подписчиков
+
+    def test_follow_posts(self):
+        """
+        Метод проверяет правильное получение постов
+        :return:
+        """
+        # создаются 4 пользователя
+        u1 = User(username='john', email='john@example.com')
+        u2 = User(username='susan', email='susan@example.com')
+        u3 = User(username='mary', email='mary@example.com')
+        u4 = User(username='david', email='david@example.com')
+        db.session.add_all([u1, u2, u3, u4])  # все созданные пользователи добавляются в БД
+
+        now = datetime.now(timezone.utc)  # фиксируется время
+        # создаются 4 поста
+        p1 = Post(body='post from john', author=u1, timestamp=now + timedelta(seconds=1))
+        p2 = Post(body='post from susan', author=u2, timestamp=now + timedelta(seconds=4))
+        p3 = Post(body='post from mary', author=u3, timestamp=now + timedelta(seconds=3))
+        p4 = Post(body='post from david', author=u4, timestamp=now + timedelta(seconds=2))
+        db.session.add_all([p1, p2, p3, p4])  # все созданные посты добавляются в БД
+        db.session.commit()  # исполнение изменений
+
+        u1.follow(u2)  # п1 подписывается на п2
+        u1.follow(u4)  # п1 подписывается на п4
+        u2.follow(u3)  # п2 подписывается на п3
+        u3.follow(u4)  # п3 подписывается на п4
+        db.session.commit()  # исполнение изменений
+
+        f1 = db.session.scalars(u1.following_posts()).all()  # получение постов для п1
+        f2 = db.session.scalars(u2.following_posts()).all()  # получение постов для п2
+        f3 = db.session.scalars(u3.following_posts()).all()  # получение постов для п3
+        f4 = db.session.scalars(u4.following_posts()).all()  # получение постов для п4
+        self.assertEqual(f1, [p2, p4, p1])  # проверка какие посты получил п1
+        self.assertEqual(f2, [p2, p3])
+        self.assertEqual(f3, [p3, p4])
+        self.assertEqual(f4, [p4])
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)  # verbosity=2 в конце кода заставит Python выводить подробный отчет по каждому тесту
+
+# для запуска тестов в терминале ввести: python tests.py
